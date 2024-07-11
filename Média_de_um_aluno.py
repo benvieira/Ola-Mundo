56f5cc3bfcf639294d8d223de7696b23f1dbd747
Ben Vieira <ben.vrchads@gmail.com>
2024-07-11 18:50:41 -0300

    Novo exercício python

--- Média_de_um_aluno.py
+++ Média_de_um_aluno.py
@@ -8,6 +8,3 @@ SIM = input ('Gostaria de saber sua média?')
 A1 = float(input('Digite sua nota A1: '))
 A2 = float(input('Agora digite sua nota A2: '))
 print('Sua média é: {}'.format((A1 + A2) / 2))
-
-
-
